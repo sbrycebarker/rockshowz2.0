6d4c86cbbff7e10f63f380e94caa214f2249cfbb
Arden <ardeneng.jr@gmail.com>
2017-07-21 15:53:02 -0600

    got things lined aroudn the middle and the navbar responsive and in the center left

--- public/js/myJQuery.js
+++ public/js/myJQuery.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   // Add scrollspy to <body>
-  $('body').scrollspy({target: ".navbar", offset: 50});   
+//   $('body').scrollspy({target: ".navbar", offset: 50});   
 
   // Add smooth scrolling on all links inside the navbar
   $("#myNavbar a").on('click', function(event) {
@@ -17,10 +17,10 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 1000, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+        window.location.hash;
       });
     }  // End if
   });
