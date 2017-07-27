5aac6c84ece70c268febf1c3da2b6230128bda47
Arden <ardeneng.jr@gmail.com>
2017-07-27 14:25:50 -0600

    removed the code for the links under the about paragraphs

--- public/js/myJQuery.js
+++ public/js/myJQuery.js
@@ -10,6 +10,14 @@ $(document).ready(function(){
                 $('#myNavbar').fadeOut(500);
             }
         });
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+    $(window).scroll(function(){                          
+            if ($(this).scrollTop() > 200) {
+                $('#myArtist').fadeIn(500);
+            } else {
+                $('#myArtist').fadeOut(500);
+            }
+        });
   // Add smooth scrolling on all links inside the navbar
   $("#myNavbar a").on('click', function(event) {
     // Make sure this.hash has a value before overriding default behavior
