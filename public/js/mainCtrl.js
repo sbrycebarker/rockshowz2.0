ec92352cbb8aee9c0a89c288aecf7dd6bc55e6fc
sbrycebarker <sbrycesti@gmail.com>
2017-09-06 15:43:27 -0600

    zip

--- public/js/mainCtrl.js
+++ public/js/mainCtrl.js
@@ -59,8 +59,8 @@ angular.module('myApp').controller('mainCtrl', function ($scope, service, auth0S
       var lat = $scope.lat
       var lng = $scope.lng
       service.getZip(lat, lng).then(function(result) {
-        console.log("zip",result.data.results[0].address_components[5])
-        var loc = result.data.results[0].address_components[5].short_name
+        console.log("zip",result.data.results[0].address_components[7])
+        var loc = result.data.results[0].address_components[7].short_name
         $scope.location = loc
       })
     }
